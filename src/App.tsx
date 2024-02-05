9e4a1b5a0287566a40e29573bebb6fd4f923dd9c
VitalGa <vitalga10@gmail.com>
2024-02-05 16:56:01 +0300

    Pizza-App-Anton

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { MouseEvent, useState } from 'react'; 
+import Button from './components/Button/Button';
+import Input from './components/Input/Input';
+import { Route, Routes } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
+
+function App() {
+	const [counter, setCounter] = useState<number>(0);
+
+	const addCounter = (e: MouseEvent) => {
+		console.log(e);
+	};
+
+	return (    
+		<>
+			<Button onClick={() => {addCounter;}}>Кнопка</Button>    
+			<Button appearance='big' onClick={addCounter}>Кнопка</Button> 
+			<Input placeholder='Email'/>
+			<div>
+				<a href="/">Меню</a>
+				<a href="cart">Корзина</a>
+			</div>
+			<Routes>
+				<Route path='/' element={<Menu/>}/>
+				<Route path='/cart' element={<Cart/>} />
+				<Route path='*' element={<Error/>} />
+			</Routes>
+		</>
+	);
+}
+
+export default App;
