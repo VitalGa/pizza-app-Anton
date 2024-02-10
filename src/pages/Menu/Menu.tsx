ef9b4980992c199931caa5286a3ba6ac2652e937
VitalGa <vitalga10@gmail.com>
2024-02-10 18:37:26 +0300

    axios

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -5,6 +5,7 @@ import Search from '../../components/Search/Search';
 import { PREFIX } from '../../helpers/API';
 import { Product } from '../../interfaces/product.interfaces';
 import styles from './Menu.module.css';
+import axios from 'axios';
 
 export function Menu() {
 
@@ -12,16 +13,12 @@ export function Menu() {
 
 	const getMenu = async () => {
 		try {
-			const res = await fetch(`${PREFIX}/products`);
-			if (!res.ok) {
-				return;
-			}
-			const data = await res.json() as Product[];
+			const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
 			setProducts(data);
-		} catch(e) {
-			console.error(e);
-			return;
-		}		
+		} catch (e) {
+			// 	console.error(e);
+		// 	return;
+		}
 	};
 
 	useEffect(() => {
