948b9426f928c82f903ebf9b8fa94761e0b994ed
VitalGa <vitalga10@gmail.com>
2024-02-15 17:36:09 +0300

    added cart

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,8 +1,18 @@
 import { Link } from 'react-router-dom';
 import styles from './ProductCard.module.css';
 import { ProductCardProps } from './ProductCard.props';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../store/store';
+import { cartAction } from '../../store/cart.slice';
 
 function ProductCard(props: ProductCardProps) {
+	const dispatch = useDispatch<AppDispatch>();
+
+	const add = (e: MouseEvent) => {
+		e.preventDefault();
+		dispatch(cartAction.add(props.id));
+	};
+
 	return (
 		<Link to={`/product/${props.id}`} className={styles['link']}>    
 			<div  className={styles['card']}>
@@ -11,7 +21,7 @@ function ProductCard(props: ProductCardProps) {
 						{props.price}&nbsp;
 						<span className={styles['currency']}>₽</span>
 					</div>
-					<button className={styles['add-to-card']}>
+					<button className={styles['add-to-card']} onClick={add}>
 						<img src="/cart-button-icon.svg" alt="Добавить в корзину" />
 					</button>
 			
--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -11,6 +11,8 @@ export function Layout() {
 	const navigate = useNavigate();
 	const dispatch = useDispatch<AppDispatch>();
 	const profile = useSelector((s: RootState) => s.user.profile);
+	const items = useSelector((s: RootState) =>s.cart.items);
+
 	useEffect(() => {
 		dispatch(getProfile());
 	}, [dispatch]);
@@ -39,6 +41,7 @@ export function Layout() {
 				})}>
 					<img src="/cart-icon.svg" alt="Иконка корзины"/>
 					Корзина</NavLink>
+				{items.reduce((acc, item) => acc += item.count, 0)}
 			</div>	
 			<Button className={styles['exit']} onClick={logout}>
 				<img src="/exit-item.svg" alt="Иконка выхода"/>
